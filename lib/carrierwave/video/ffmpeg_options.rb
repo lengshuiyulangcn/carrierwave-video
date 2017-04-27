c04d1d66916289fc7138aa39806800a92d866b32
gyorou <gyorou@tjjtds.me>
2017-04-27 14:50:12 +0900

    modify convert confgi

--- lib/carrierwave/video/ffmpeg_options.rb
+++ lib/carrierwave/video/ffmpeg_options.rb
@@ -81,8 +81,7 @@ module CarrierWave
             case format
             when 'mp4'
               h[:video_codec] = 'libx264'
-              h[:audio_codec] = 'libfaac'
-              h[:custom] = '-qscale 0 -preset slow -g 30'
+              h[:custom] = "-strict experimental -preset slow -g 30"
             when 'ogv'
               h[:video_codec] = 'libtheora'
               h[:audio_codec] = 'libvorbis'
